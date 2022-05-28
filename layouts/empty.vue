a3030307806935148866164fb0a7ee459d7c9c79
Egor Vdovenko <egor.vdovenko@roskvartal.ru>
2022-05-28 14:04:00 +0300

    feat: index page

---
+++ layouts/empty.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-app class="root">
+    <Nuxt />
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'EmptyLayout'
+}
+</script>
+
+<style lang="scss" scoped>
+.root {
+  height: 100vh;
+  width: 100vw;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,83 +1,56 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <v-card class="logo py-4 d-flex justify-center">
-        <NuxtLogo />
-        <VuetifyLogo />
-      </v-card>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
-          >
-            Continue
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-col>
-  </v-row>
+  <div class="root">
+    <h1 class="app-title">
+      Trippicker
+    </h1>
+    <div class="half-part" />
+    <v-btn
+      to="/map"
+      elevation="2"
+      tile
+      x-large
+    >
+      Pick the trip
+    </v-btn>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+  layout: 'empty'
 }
 </script>
+
+<style lang="scss" scoped>
+.root {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-image: url("~/assets/images/landing-background.webp");
+}
+
+.half-part {
+  position: absolute;
+  top: 0;
+  left: 0;
+  clip-path: polygon(0 0, 100% 0%, 0 100%, 0% 100%);
+  height: 100%;
+  width: 100%;
+  background-color: rgba(0, 0, 0, 0.25);
+}
+
+.app-title {
+  position: absolute;
+  top: 0;
+  left: 0;
+  line-height: 28px;
+  font-size: 28px;
+  font-weight: 100;
+  padding: 8px;
+  letter-spacing: 4px;
+  z-index: 1;
+}
+</style>
