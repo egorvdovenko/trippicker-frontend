3f7130d4e2f79db1f4253c173b0107a673c80af5
Egor Vdovenko <egor.vdovenko@roskvartal.ru>
2022-05-29 13:13:05 +0300

    feat (api): PlacesController additional methods

--- api/controllers/Places.controller.js
+++ api/controllers/Places.controller.js
@@ -10,6 +10,28 @@ class PlacesController extends BaseController {
       params: payload
     })
   }
+
+  getPlacesList (payload) {
+    return this.axios({
+      method: 'get',
+      url: `${PlacesController.prefix}/list`,
+      params: payload
+    })
+  }
+
+  getPlacesListItems () {
+    return this.axios({
+      method: 'get',
+      url: `${PlacesController.prefix}/listItems`
+    })
+  }
+
+  getPlace (id) {
+    return this.axios({
+      method: 'get',
+      url: `${PlacesController.prefix}/${id}`
+    })
+  }
 }
 
 export default PlacesController
