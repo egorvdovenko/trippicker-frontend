c4e1bc6ccb0d897c0098c677b7ca3944eb1fc6eb
Egor Vdovenko <egor.vdovenko@roskvartal.ru>
2022-05-29 13:11:27 +0300

    feat: AppLoader component

---
+++ components/AppLoader.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="root">
+    <h2 class="title">
+      <slot name="title" />
+    </h2>
+    <v-progress-circular
+      :size="30"
+      :width="3"
+      color="#282828"
+      indeterminate
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppLoader'
+}
+</script>
+
+<style lang="scss" scoped>
+.root {
+  height: 100%;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  color: #282828;
+}
+
+.title {
+  margin-bottom: 16px;
+}
+</style>
